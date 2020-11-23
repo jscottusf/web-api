523fb8d39094a927967e7d78903f0f4c5e0980ba
Joel Scott <JKScott@tql.com>
2020-11-23 14:53:03 -0500

    adding initial files

---
+++ web-api-cs/Controllers/CampsController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_api_cs.Models;
+
+namespace web_api_cs.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+
+                return _mapper.Map<CampModel[]>(results);
+            }
+
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database 500 Error");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database 500 Error");
+            }
+        }
+    }
+}
---
+++ web-api-cs/Data/CampProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_api_cs.Models;
+
+namespace web_api_cs.Data
+{
+    public class CampProfile: Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>();
+        }
+    }
+}
